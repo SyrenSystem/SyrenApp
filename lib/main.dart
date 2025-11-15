467d9aac6f5078e0379869fae029b7f372497a75
Laurens Grote <lau.grote.19@lehre.mosbach.dhbw.de>
2025-11-15 15:16:25 +0100

    added serial port reading on android; not fully implemented yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:libserialport/libserialport.dart';
+import 'package:usb_serial/usb_serial.dart';
+import 'dart:typed_data';
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
+
   runApp(const MyApp());
 }
 
@@ -27,44 +32,41 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
   late SerialPort _serialPort;
+
   int _selectedIndex = 0;
   bool _isPlaying = false;
 
   @override
   void initState() {
     super.initState();
-    final ports = SerialPort.availablePorts;
-    print('Available ports: $ports');
-
-    _serialPort = SerialPort(ports.first);
-    if (!_serialPort.openRead()) {
-      print('failed to open port');
-    }
-
-    final config = SerialPortConfig();
-    config.baudRate = 115200;
-    config.bits = 8;
-    config.stopBits = 1;
-    config.parity = 0;
-    _serialPort.config = config;
-
-    final reader = SerialPortReader(_serialPort);
-    String buffer = '';
-
-    reader.stream.listen((data) {
-      final text = String.fromCharCodes(data);
-      int index = text.indexOf("\t\n");
-      if (index > 0) {
-
-      }
-
-      buffer += text;
-      if (buffer.contains("\t\n")) {
-        print("received: $buffer");
-        buffer = '';
-      }
-    });
+    // final ports = SerialPort.availablePorts;
+    // print('Available ports: $ports');
+    //
+    // _serialPort = SerialPort(ports.first);
+    // if (!_serialPort.openRead()) {
+    //   print('failed to open port');
+    // }
+    //
+    // final config = SerialPortConfig();
+    // config.baudRate = 115200;
+    // config.bits = 8;
+    // config.stopBits = 1;
+    // config.parity = 0;
+    // _serialPort.config = config;
+    //
+    // final reader = SerialPortReader(_serialPort);
+    // String buffer = '';
+    //
+    // reader.stream.listen((data) {
+    //   final text = String.fromCharCodes(data);
+    //   buffer += text;
+    //   if (buffer.contains("\r\n")) {
+    //     print("received: $buffer");
+    //     buffer = '';
+    //   }
+    // });
   }
 
   @override
@@ -126,41 +128,85 @@ class _MainPageState extends State<MainPage> {
             child: Stack(
               alignment: Alignment.center,
               children: [
-                Positioned(
-                  top: 40,
-                  left: MediaQuery.of(context).size.width / 2 - 20,
-                  child: const Icon(
-                    Icons.music_note,
-                    size: 40,
-                    color: Colors.red,
-                  ),
-                ),
-                Positioned(
-                  bottom: 120,
-                  left: 40,
-                  child: const Icon(
-                    Icons.music_note,
-                    size: 40,
-                    color: Colors.red,
-                  ),
-                ),
-                Positioned(
-                  bottom: 50,
-                  right: 40,
-                  child: const Icon(
-                    Icons.music_note,
-                    size: 40,
-                    color: Colors.red,
-                  ),
-                ),
-                Container(
-                  width: 40,
-                  height: 40,
-                  decoration: const BoxDecoration(
-                    color: Colors.blue,
-                    shape: BoxShape.circle,
-                  ),
-                ),
+
+                ElevatedButton(
+                    child: Text(
+                      'M',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onPressed: () async {
+                      print("TEST from button m");
+                        List<UsbDevice> devices = await UsbSerial.listDevices();
+                        print(devices);
+
+                        UsbPort? port;
+                        if (devices.length == 0) {
+                          return;
+                        }
+                        port = await devices[0].create();
+
+                        if (port != null)
+                          {
+                            bool openResult = await port.open();
+                            if ( !openResult ) {
+                              print("Failed to open");
+                              return;
+                            }
+
+                            await port.setDTR(true);
+                            await port.setRTS(true);
+
+                            port.setPortParameters(115200, UsbPort.DATABITS_8,
+                                UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+                            // print first result and close port.
+                            port.inputStream!.listen((Uint8List event) {
+                              String text = utf8.decode(event);
+                              print(text);
+                            });
+                            Timer (const Duration(seconds: 5), (){
+                              port!.close();
+                            });
+                          }
+
+                    },
+                  )
+
+                // Positioned(
+                //   top: 40,
+                //   left: MediaQuery.of(context).size.width / 2 - 20,
+                //   child: const Icon(
+                //     Icons.music_note,
+                //     size: 40,
+                //     color: Colors.red,
+                //   ),
+                // ),
+                // Positioned(
+                //   bottom: 120,
+                //   left: 40,
+                //   child: const Icon(
+                //     Icons.music_note,
+                //     size: 40,
+                //     color: Colors.red,
+                //   ),
+                // ),
+                // Positioned(
+                //   bottom: 50,
+                //   right: 40,
+                //   child: const Icon(
+                //     Icons.music_note,
+                //     size: 40,
+                //     color: Colors.red,
+                //   ),
+                // ),
+                // Container(
+                //   width: 40,
+                //   height: 40,
+                //   decoration: const BoxDecoration(
+                //     color: Colors.blue,
+                //     shape: BoxShape.circle,
+                //   ),
+                // ),
               ],
             ),
           ),
